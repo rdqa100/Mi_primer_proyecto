06ac3c347a4589bad4b86d9136c7cea0c77a7fd8
David <rubendqa@gmail.com>
2023-06-19 21:41:30 -0400

    primer seguimiento

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("imagen").src="https://rickandmortyapi.com/api/character/avatar/76.jpeg"
+function cambiarImagen1() {
+    document.getElementById("imagen").src="https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+}
+function cambiarImagen2() {
+    document.getElementById("imagen").src="https://rickandmortyapi.com/api/character/avatar/76.jpeg"
+}
+
